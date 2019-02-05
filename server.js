a467a3ba1d18c9151d71bacbe210b6163a41346a
dmithamo1 <dennis.mithamo@andela.com>
2019-02-05 10:09:01 +0300

    fx(fix failing deploy): fix crashing Heroku app

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const APP = express();
 
 APP.use(express.static(`${__dirname}/dist`));
+
+APP.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
 APP.listen(process.env.PORT || 8080);
